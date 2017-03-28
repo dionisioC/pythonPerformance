c618c6c9a52aca3d0c66fccaff20c1cd63baf306
Dionisio Cortes Fernandez <cortesfernandezdionisio@gmail.com>
2017-03-28 22:33:56 +0200

    Fix slides and generator examples

--- examples/02 - Generators/generators.py
+++ examples/02 - Generators/generators.py
@@ -7,13 +7,15 @@ my_config = config.get_config()
 number_of_loops = my_config.getint("Global", "number_of_loops")
 
 
-def generator():
-    return [x*2 for x in range(number_of_loops)]
+def my_generator(start_value):
+    while start_value < number_of_loops:
+        yield start_value
+        start_value += 1
 
 
 @profile
 def generator_testing():
-    for loop_count in generator():
+    for loop_count in my_generator(0):
         foo = loop_count + 3
 
 start = time.time()
